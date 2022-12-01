e3a2f162512d54a6dc7f94ea217660584592350d
Brandon <thegamingcop2005@gmail.com>
2022-12-01 12:57:44 +0200

    Make it neat

--- 2022/day1/solution.py
+++ 2022/day1/solution.py
@@ -1,7 +1,6 @@
 with open("input.txt") as f:
     elves_bags = f.read().splitlines()
 
-# Part 1
 calories_tracker: list[int] = []
 current_calories = 0
 for calories in elves_bags:
@@ -12,16 +11,8 @@ for calories in elves_bags:
 
     current_calories += int(calories)
 
+# Part 1
+print(max(calories_tracker))
 
 #Part 2
-calories_tracker: list[int] = []
-current_calories = 0
-for calories in elves_bags:
-    if calories == '':
-        calories_tracker.append(current_calories)
-        current_calories = 0
-        continue
-    
-    current_calories += int(calories)
-
 print(sum(sorted(calories_tracker, reverse=True)[:3]))
