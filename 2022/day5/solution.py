c318ac5b3314e876bf15d7ca246efc698aaf600e
Brandon <thegamingcop2005@gmail.com>
2022-12-06 02:10:39 +0200

    Cleanup

--- 2022/day5/solution.py
+++ 2022/day5/solution.py
@@ -4,7 +4,7 @@ from collections import deque
 with open("input.txt") as f:
     crates, instructions = f.read().split("\n\n")
 
-def part_1():
+def parse_stacks() -> list[deque]:
     stacks: list[deque] = []
     for line in crates.splitlines():
         for i, idx in enumerate(range(1, len(line), 4)):
@@ -13,27 +13,22 @@ def part_1():
 
             if line[idx] != " ":
                 stacks[i].append(line[idx])
-    print(stacks)
+
+    return stacks
+
+def part_1():
+    stacks = parse_stacks()
     for instruction in instructions.splitlines():
         a, b, c = map(int, re.findall(r"\d+", instruction))
         for _ in range(a):
-            print(f"Moving {a} from {b} to {c}")
             stacks[c - 1].appendleft(stacks[b - 1].popleft())
+            print(f"Moving {a} from {b} to {c}")
             print(stacks)
 
     return "".join(stack.popleft() for stack in stacks)
 
 def part_2():
-    stacks: list[deque] = []
-    for line in crates.splitlines():
-        for i, idx in enumerate(range(1, len(line), 4)):
-            while i >= len(stacks):
-                stacks.append(deque())
-            
-            if line[idx] != " ":
-                stacks[i].append(line[idx])
-
-    print(stacks)
+    stacks = parse_stacks()
     for instruction in instructions.splitlines():
         a, b, c = map(int, re.findall(r"\d+", instruction))
         temp = deque()
