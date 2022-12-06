231855750f660747a0265e2733d4476ec60db50d
Brandon <thegamingcop2005@gmail.com>
2022-12-06 18:01:27 +0200

    Day 6 use sets

--- 2022/day6/solution.py
+++ 2022/day6/solution.py
@@ -3,26 +3,17 @@ with open("input.txt") as f:
 
 def part_1() -> int:
     for i in range(len(stream)):
-        repeated = False
-        for char in stream[i:i + 4]:
-            if stream[i:i + 4].count(char) > 1:
-                repeated = True
-                continue
-
-        if not repeated:
+        if len(set(stream[i:i + 4])) == 4:
             return i + 4
 
     return 0
 
 def part_2() -> int:
     for i in range(len(stream)):
-        repeated = False
-        for char in stream[i:i+14]:
-            if stream[i:i+14].count(char) > 1:
-                repeated = True
-                continue
-
-        if not repeated:
+        if len(set(stream[i:i+14])) == 14:
             return i + 14
 
     return 0
+
+print(part_1())
+print(part_2())
